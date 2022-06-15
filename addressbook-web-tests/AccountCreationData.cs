79f0c8775aca4cc7736e9ceb4d6382f02248330b
Дмитрий Иванов <randomsize@narod.ru>
2022-06-15 17:26:19 +0400

    AccountCreationComplete

--- addressbook-web-tests/AccountCreationData.cs
+++ addressbook-web-tests/AccountCreationData.cs
@@ -117,9 +117,204 @@ namespace addressbook_web_tests
                 address = value;
             }
         }
-
-
-
+        public string Home 
+        {
+            get 
+            {
+                return home;
+            }
+            set 
+            {
+                home = value;
+            }
+        }
+        public string Mobile 
+        {
+            get 
+            {
+                return mobile;
+            }
+            set 
+            {
+                mobile = value;
+            }
+        }
+        public string Work 
+        {
+            get 
+            {
+                return work;
+            }
+            set 
+            {
+                work = value; 
+            }
+        }
+        public string Fax 
+        {
+            get 
+            {
+                return fax; 
+            }
+            set 
+            {
+                fax = value; 
+            }
+        }
+        public string Email 
+        {
+            get 
+            {
+                return email; 
+            }
+            set 
+            {
+                email = value; 
+            }
+        }
+        public string Email2 
+        {
+            get 
+            {
+                return email2; 
+            }
+            set 
+            {
+                email2 = value; 
+            }
+        }
+        public string Email3
+        {
+            get
+            {
+                return email3;
+            }
+            set
+            {
+                email3 = value;
+            }
+        }
+        public string Homepage
+        {
+            get
+            {
+                return homepage;
+            }
+            set
+            {
+                homepage = value;
+            }
+        }
+        public string Bday
+        {
+            get
+            {
+                return bday;
+            }
+            set
+            {
+                bday = value;
+            }
+        }
+        public string Bmonth
+        {
+            get
+            {
+                return bmonth;
+            }
+            set
+            {
+                bmonth = value;
+            }
+        }
+        public string Byear
+        {
+            get
+            {
+                return byear;
+            }
+            set
+            {
+                byear = value;
+            }
+        }
+        public string Aday
+        {
+            get
+            {
+                return aday;
+            }
+            set
+            {
+                aday = value;
+            }
+        }
+        public string Amonth
+        {
+            get
+            {
+                return amonth;
+            }
+            set
+            {
+                amonth = value;
+            }
+        }
+        public string Ayear
+        {
+            get
+            {
+                return ayear;
+            }
+            set
+            {
+                ayear = value;
+            }
+        }
+        public string New_group
+        {
+            get
+            {
+                return new_group;
+            }
+            set
+            {
+                new_group = value;
+            }
+        }
+        public string Address2
+        {
+            get
+            {
+                return address2;
+            }
+            set
+            {
+                address2 = value;
+            }
+        }
+        public string Phone2
+        {
+            get
+            {
+                return phone2;
+            }
+            set
+            {
+                phone2 = value;
+            }
+        }
+        public string Notes
+        {
+            get
+            {
+                return notes;
+            }
+            set
+            {
+                notes = value;
+            }
+        }
     }
     }
 
